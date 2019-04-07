69cb7a583d555844509a714651a5d123d004a17b
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-07 16:42:01 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,44 +1,59 @@
-#include <netdb.h> 
-#include <stdio.h> 
-#include <unistd.h>
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h>
-#define PORT 5000 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
-int main(int argc , char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+//   SERVER   
+
+int main(int argc, char *argv[])
 {
-     int sockfd = 0, vread;
-     char *hello = "hello from client";
-     char buff[1000] = {0};
-     struct sockaddr_in adrress;
-     struct sockaddr_in serv_addr;
+        //Declaring process variables.
+        int server_sockfd, client_sockfd;
+        int server_len ; 
+        int intBind; 
+        unsigned client_len;
+        struct sockaddr_in server_address;
+        struct sockaddr_in client_address;
+
+        //Remove any old socket and create an unnamed socket for the server.
+        server_sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+        server_address.sin_family = AF_INET;
+        server_address.sin_addr.s_addr = htons(INADDR_ANY);
+        server_address.sin_port = htons(7734) ; 
+        server_len = sizeof(server_address);
+
+        intBind = bind(server_sockfd, (struct sockaddr *) &server_address, server_len);
+        printf("RC from bind = %d\n", intBind ) ; 
+
+        char *msgFromServer = "Successfull response from Server";
+        char *msgFromClient = "";
+
+        //Create a connection queue and wait for clients
+        intBind = listen(server_sockfd, 5);
+        printf("RC from listen = %d\n", intBind ) ; 
+
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, (struct sockaddr *) &client_address, &client_len);
+        printf("after accept()... client_sockfd = %d\n", client_sockfd) ; 
+
+        printf("server waiting\n");
+
+        msgFromClient = read(client_sockfd, &msgFromClient, 1);
+        printf("RC from read = %d\n", intBind ) ;            
+
+        write(client_sockfd, &msgFromServer, 1);
+
+        printf("server exiting\n");
+
      
-     //create socket
-     if ((sockfd = socket(AF_INET, SOCK_STREAM,0)) < 0)
-     {
-          printf ("could not create socket");
-	  return -1;
-     }
-     memset(&serv_addr, '0', sizeof(serv_addr));
-     
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_port = htons(5000);
-     
-     if (inet_pton(AF_INET, "192.168.184.131", &serv_addr.sin_addr) <= 0)
-     {
-          //print the error message
-          printf("\n it's error \n");
-          return -1;
-     }
-	
-	//Accept and incoming connection
-	send(sockfd, hello, strlen(hello) , 0);
-	  printf("hello message sent");
-	  vread = read(sockfd, buff , 1000);
-	  printf("%s\n" , buff);
-	  
-	
-	return 0;
+        close(client_sockfd);
+        return 0;
 }
