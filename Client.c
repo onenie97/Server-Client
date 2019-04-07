bbbe5a50e317dd6286bac19cc912492a86f51de8
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-07 17:04:38 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,59 +1,32 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 
-//   SERVER   
+#define PORT 4444
 
-int main(int argc, char *argv[])
-{
-        //Declaring process variables.
-        int server_sockfd, client_sockfd;
-        int server_len ; 
-        int intBind; 
-        unsigned client_len;
-        struct sockaddr_in server_address;
-        struct sockaddr_in client_address;
+void main(){
+	
+	int clientSocket;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
 
-        //Remove any old socket and create an unnamed socket for the server.
-        server_sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	printf("[+]Client Socket Created Sucessfully.\n");
 
-        server_address.sin_family = AF_INET;
-        server_address.sin_addr.s_addr = htons(INADDR_ANY);
-        server_address.sin_port = htons(7734) ; 
-        server_len = sizeof(server_address);
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-        intBind = bind(server_sockfd, (struct sockaddr *) &server_address, server_len);
-        printf("RC from bind = %d\n", intBind ) ; 
+	connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	printf("[+]Connected to Server.\n");
 
-        char *msgFromServer = "Successfull response from Server";
-        char *msgFromClient = "";
+	recv(clientSocket, buffer, 1024, 0);
+	printf("[+]Data Recv: %s\n", buffer);
+	
 
-        //Create a connection queue and wait for clients
-        intBind = listen(server_sockfd, 5);
-        printf("RC from listen = %d\n", intBind ) ; 
-
-        client_len = sizeof(client_address);
-        client_sockfd = accept(server_sockfd, (struct sockaddr *) &client_address, &client_len);
-        printf("after accept()... client_sockfd = %d\n", client_sockfd) ; 
-
-        printf("server waiting\n");
-
-        msgFromClient = read(client_sockfd, &msgFromClient, 1);
-        printf("RC from read = %d\n", intBind ) ;            
-
-        write(client_sockfd, &msgFromServer, 1);
-
-        printf("server exiting\n");
-
-     
-        close(client_sockfd);
-        return 0;
 }
