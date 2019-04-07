2f425db66df90081aebca3e6a798804521c21e7e
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-07 15:08:40 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,62 +1,86 @@
-import java.net.*; 
-import java.io.*; 
-  
-public class Server 
-{ 
-    //initialize socket and input stream 
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-  
-    // constructor with port 
-    public Server(int port) 
-    { 
-        // starts server and waits for a connection 
+import java.net.*;
+import java.io.*;
+
+class Build implements Runnable
+{
+    int  port = 3500;
+    String input1;
+    ServerSocket server;
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    Console reader;
+
+    public void work()
+    {
         try
-        { 
-            server = new ServerSocket(5000); 
-            System.out.println("Server started"); 
-  
-            System.out.println("Waiting for a client ..."); 
-            System.out.println("Client accepted"); 
-            System.out.println("Hello");
-  
-            socket = server.accept(); 
-            
-            // takes input from the client socket 
-            in = new DataInputStream( 
-                new BufferedInputStream(socket.getInputStream())); 
-  
-            String line = ""; 
-  
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) 
-            { 
-                try
-                { 
-                    line = in.readUTF(); 
-                    System.out.println(line); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("What's up!"); 
-  
-            // close connection 
-            socket.close(); 
-            in.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Server server = new Server(5000); 
-    } 
+        {
+            server = new ServerSocket(port);
+            socket = server.accept();
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            reader = System.console();
+        }
+
+        catch(IOException e){
+        e.printStackTrace();}
+    }
+
+    public void speak()
+    {
+        try
+        {
+            while(true)
+            {
+                input1 = reader.readLine();
+                out.writeUTF(input1);
+                if(input1.equals("stop"))
+                {
+                    in.close();
+                    out.close();
+                    socket.close();
+                    server.close();
+                    System.exit(0);
+                }               
+            }
+        }
+        catch (IOException e){
+        e.printStackTrace();}
+    }
+
+    public void run()
+    {
+        String input2;
+
+        try
+        {
+            while(true)
+            {
+                input2 = in.readUTF();
+                System.out.println(input2);
+                if(input2.equals("stop"))
+                {
+                    in.close();
+                    out.close();
+                    socket.close();
+                    server.close();
+                    System.exit(0);
+                }
+             }
+         }
+        catch(IOException e){
+        e.printStackTrace();}
+     }
+}
+
+public class server
+{
+    public static void main(String[]args)
+    {
+        Build object = new Build();
+        object.work();
+        Thread t1 = new Thread(object);
+        t1.start();
+        object.speak();
+    }
 }
