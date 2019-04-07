de2847066fcfd3c36dcab6d08e3ba98e8d7fd1f9
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-07 15:50:05 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,86 +1,31 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-class Build implements Runnable
-{
-    int  port = 3500;
-    String input1;
-    ServerSocket server;
-    Socket socket;
-    DataInputStream in;
-    DataOutputStream out;
-    Console reader;
-
-    public void work()
-    {
-        try
-        {
-            server = new ServerSocket(port);
-            socket = server.accept();
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            reader = System.console();
-        }
-
-        catch(IOException e){
-        e.printStackTrace();}
-    }
-
-    public void speak()
-    {
-        try
-        {
-            while(true)
-            {
-                input1 = reader.readLine();
-                out.writeUTF(input1);
-                if(input1.equals("stop"))
-                {
-                    in.close();
-                    out.close();
-                    socket.close();
-                    server.close();
-                    System.exit(0);
-                }               
+class server{
+    public static void main(String[] args) {
+        try{
+            
+            ServerSocket ss = new ServerSocket(1201);
+            Socket s =ss.accept();
+            
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            String msgin= "",msgout="";
+            
+            while(!msgin.equals("end")){
+                msgin = din.readUTF();
+                System.out.println(msgin);
+                msgout = br.readLine();
+                dout.writeUTF(msgout);
+                dout.flush();
             }
+            s.close();
+            
+        }catch(Exception e){
+            //handle exceptions
         }
-        catch (IOException e){
-        e.printStackTrace();}
     }
-
-    public void run()
-    {
-        String input2;
-
-        try
-        {
-            while(true)
-            {
-                input2 = in.readUTF();
-                System.out.println(input2);
-                if(input2.equals("stop"))
-                {
-                    in.close();
-                    out.close();
-                    socket.close();
-                    server.close();
-                    System.exit(0);
-                }
-             }
-         }
-        catch(IOException e){
-        e.printStackTrace();}
-     }
-}
-
-public class server
-{
-    public static void main(String[]args)
-    {
-        Build object = new Build();
-        object.work();
-        Thread t1 = new Thread(object);
-        t1.start();
-        object.speak();
-    }
-}
+                
