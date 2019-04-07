5f8b5519ca6a19560ecfbb1e6b6da7cfdb1fe21a
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-07 17:47:20 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -8,25 +8,34 @@
 
 #define PORT 4444
 
-void main(){
+int main(){
 	
-	int clientSocket;
+	int clientSocket, ret;
 	struct sockaddr_in serverAddr;
-	char buffer[1024];
+	char buffer[1201];
 
 	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (clientSocket < 0){
+		printf("[+]Error in connection.\n");
+		exit(1);
+	}
 	printf("[+]Client Socket Created Sucessfully.\n");
 
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(4444);
+	serverAddr.sin_port = htons(PORT);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	printf("[+]Connected to Server.\n");
+	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("[+]Error in connection.\n");
+		exit(1);
+	}
 
-	recv(clientSocket, buffer, 1024, 0);
+	recv(clientSocket, buffer, 1201, 0);
 	printf("[+]Data Recv: %s\n", buffer);
+	
+	return 0;
 	
 
 }
