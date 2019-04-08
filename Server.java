8841ffcc52fea66e7df8aa9297e5574fe8309618
onenie97 <48428857+onenie97@users.noreply.github.com>
2019-04-08 10:57:09 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,37 +1,32 @@
 import java.io.*;
 import java.net.*;
-
-class Server
+public class GossipServer
 {
-    public static void main(String[] args) 
-    {
-        try
+  public static void main(String[] args) throws Exception
+  {
+      ServerSocket sersock = new ServerSocket(3000);
+      System.out.println("Server  ready for chatting");
+      Socket sock = sersock.accept( );                          
+                              // reading from keyboard (keyRead object)
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      // sending to client (pwrite object)
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
+ 
+                              // receiving from server ( receiveRead  object)
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
         {
-            
-            ServerSocket ss = new ServerSocket(1201);
-            Socket s =ss.accept();
-            
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
-            String msgin= "",msgout="";
-            
-            while(!msgin.equals("end"))
-            {
-                msgin = din.readUTF();
-                System.out.println(msgin);
-                msgout = br.readLine();
-                dout.writeUTF(msgout);
-                dout.flush();
-            }
-            s.close();
-            
-        }
-        catch(Exception e)
-        {
-            //handle exceptions
-        }
-    }
-}
+           System.out.println(receiveMessage);         
+        }         
+        sendMessage = keyRead.readLine(); 
+        pwrite.println(sendMessage);             
+        pwrite.flush();
+      }               
+    }                    
+}                        
